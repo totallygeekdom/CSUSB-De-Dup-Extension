ac8446fc11360b8b8f45accaef9c0ab553f4cede
totallygeekdom <76084843+totallygeekdom@users.noreply.github.com>
2026-02-12 08:35:14 -0800

    Update version number to 121.5

--- Element451-UI-Perfection-v119.0.user.js
+++ Element451-UI-Perfection-v119.0.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Element451 - UI Perfection
 // @namespace    http://tampermonkey.net/
-// @version      121.4
+// @version      121.5
 // @description  Merge workflow with auto-selection, smart links, and UI enhancements (manual FAB click required)
 // @author       You
 // @match        https://*.element451.io/*
